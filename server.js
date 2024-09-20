933c4752f59ac41892cc8b0fb3ae651d62356576
Nipun Prijanjith <133010944+nipun-priyanjith@users.noreply.github.com>
2024-09-20 18:05:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // Ensure dotenv is required before connectDB
 const express = require('express');
 const cors = require('cors'); // Import cors
 const connectDB = require('./config/db');
